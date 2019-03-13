76fe5b5f238d7545158d040cc0348d67acbbc997
jq-rs <jq-rs@mles.io>
2019-03-13 07:10:25 +0200

    Update webworker.js

--- js/webworker.js
+++ js/webworker.js
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * Copyright (c) 2019 Mles WebWorker developers
+ * Copyright (c) 2019 MlesTalk WebWorker developers
  */
 
 importScripts('cbor.js', 'blake2s.js', 'blowfish.js');
